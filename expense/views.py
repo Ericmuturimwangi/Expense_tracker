68370bc6e855576b86063764ca2907dcbbc205c5
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-12-06 20:19:45 +0300

    backend of the adding and listing expenses

--- expense/views.py
+++ expense/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Expense
+from .forms import ExpenseForm
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def expense_list(request):
+    expenses = Expense.objects.filter(user = request.user)
+    return render(request, 'expense/expense_list.html', {'expense': expenses})
+
+def add_expense(request):
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render (request, 'expense/add_expense.html', {'form':form})
+
